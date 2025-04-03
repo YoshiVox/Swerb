e396129983274ebea325889566374fe4566aa319
Yajie Li <104532860+YoshiVox@users.noreply.github.com>
2025-04-03 09:00:32 -0500

    update

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/swerveModule.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.arcrobotics.ftclib.geometry.Translation2d;
+import com.arcrobotics.ftclib.kinematics.wpilibkinematics.SwerveDriveKinematics;
+
+public class swerveModule {
+    Translation2d m_frontLeftLocation =
+            new Translation2d(0.381, 0.381);
+    Translation2d m_frontRightLocation =
+            new Translation2d(0.381, -0.381);
+    Translation2d m_backLeftLocation =
+            new Translation2d(-0.381, 0.381);
+    Translation2d m_backRightLocation =
+            new Translation2d(-0.381, -0.381);
+
+    SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics
+            (
+                    m_frontLeftLocation, m_frontRightLocation,
+                    m_backLeftLocation, m_backRightLocation
+            );
+}
